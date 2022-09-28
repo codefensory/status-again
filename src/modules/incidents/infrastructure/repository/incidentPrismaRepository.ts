0447447a435358436b64bbd64cd4c5ad0fe9c4e7
Yeferson Mejias <yefer.el.teke@gmail.com>
2022-09-28 16:09:16 -0500

    chore: find monitor in repository

--- src/modules/incidents/infrastructure/repository/incidentPrismaRepository.ts
+++ src/modules/incidents/infrastructure/repository/incidentPrismaRepository.ts
@@ -1,11 +1,17 @@
 import { Incident } from "@prisma/client";
-import { Result } from "oxide.ts";
+import { Err, Result } from "oxide.ts";
 import { prisma } from "../../../shared/database";
 import { ToCreate } from "../../../shared/utils/types";
 import { IncidentRepository } from "../../domain";
 
 class IncidentPrismaRepository implements IncidentRepository {
   async createIncident(incident: ToCreate<Incident>): Promise<Result<Incident, Error>> {
+    const monitor = await prisma.monitor.findFirst({ where: { id: incident.monitorId } });
+
+    if (!monitor) {
+      return Err(new Error("Monitor not exists"));
+    }
+
     return Result(await prisma.incident.create({ data: incident }));
   }
 }
